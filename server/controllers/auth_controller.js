87ed6342f90a32d757c3cf17d66654e48862af26
Dylan Lee <csleedylan@gmail.com>
2019-03-07 20:56:20 -0600

    l

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -31,12 +31,12 @@ module.exports = {
       id: mortal.mortal_id,
       number: mortal.number
     };
-    const twiml = new MessagingResponse();
-    twiml.message(
-      "Thank you for creating an account at The Productivity Place! I will be your mobile assistant to help keep you productive. Say somethings along the lines of What can you do for me? or What all can you do? To get some help in learning how to use my service :)"
-    );
-    resp.writeHead(200, { "Content-Type": "text/xml" });
-    resp.end(twiml.toString());
+    // const twiml = new MessagingResponse();
+    // twiml.message(
+    //   "Thank you for creating an account at The Productivity Place! I will be your mobile assistant to help keep you productive. Say somethings along the lines of What can you do for me? or What all can you do? To get some help in learning how to use my service :)"
+    // );
+    // resp.writeHead(200, { "Content-Type": "text/xml" });
+    // resp.end(twiml.toString());
     db.default_settings(mortal.mortal_id);
     return res.status(200).json(req.session.user);
   },
